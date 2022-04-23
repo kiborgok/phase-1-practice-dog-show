249a53cba4ec2dc1521ac22c9e343b9dba02ded6
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-23 16:18:29 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const dogForm = document.getElementById("dog-form");
+        const tbody = document.getElementById("table-body");
+        
+  dogForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(e.target.children.name.id)
+    const id = parseInt(e.target.children.name.id);
+    const name = e.target.children.name.value;
+    const sex = e.target.children.sex.value;
+    const breed = e.target.children.breed.value;
+    const dogObject = {
+      name,
+      sex,
+      breed,
+    };
+      editDog(id, dogObject).then(dogData => {
+          fetchDogs.then(dogsData => {
+              const newDogsData = [...dogsData, dogData]
+              tbody.textContent = "";
+              for (dog of newDogsData) {
+                createDogData(dog);
+              }
+          })
+      })
+  });
 
-})
+  function fetchDogs() {
+    return fetch(`http://localhost:3000/dogs`).then((res) => res.json());
+  }
+  function editDog(id, dogObject) {
+    return fetch(`http://localhost:3000/dogs/${id}`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        },
+      body:JSON.stringify(dogObject)
+    }).then((res) => res.json());
+  }
+
+  displayAllDogs();
+  function displayAllDogs() {
+    fetchDogs().then((data) => {
+      for (dog of data) {
+        createDogData(dog);
+      }
+    });
+  }
+  function createDogData(data) {
+  
+    const row = document.createElement("tr");
+    const dogName = document.createElement("td");
+    const dogBreed = document.createElement("td");
+    const dogSex = document.createElement("td");
+    const dogEdit = document.createElement("td");
+    dogEdit.style.cursor = "pointer";
+    dogEdit.textContent = "Edit Dog";
+    dogEdit.addEventListener("click", (e) => {
+      const name = e.target.parentNode.children[0].textContent;
+      const breed = e.target.parentNode.children[1].textContent;
+      const sex = e.target.parentNode.children[2].textContent;
+      dogForm.children.name.setAttribute("id", data.id);
+      dogForm.children.name.value = name;
+      dogForm.children.sex.value = sex;
+      dogForm.children.breed.value = breed;
+    });
+    dogName.textContent = data.name;
+    dogBreed.textContent = data.breed;
+    dogSex.textContent = data.sex;
+    row.appendChild(dogName);
+    row.appendChild(dogBreed);
+    row.appendChild(dogSex);
+    row.appendChild(dogEdit);
+    tbody.appendChild(row);
+  }
+});
